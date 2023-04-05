2763aa99492951c520881440c4c6437c474d37d9
makwaje <alloushi2010@gmail.com>
2023-04-05 21:29:39 +0200

    first commit

---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,18 @@
+import "./card.styles.css";
+
+const Card = ({ monster }) => {
+  const { id, email, name } = monster;
+
+  return (
+    <div className="card-container" key={{ id }}>
+      <img
+        alt={`Monster ${name}`}
+        src={`https://robohash.org/${id}?set=set2&size=180x180`}
+      />
+      <h2>{name}</h2>
+      <p>{email}</p>
+    </div>
+  );
+};
+
+export default Card;
